69fdb34c3d3c07831f1b2ff4801e45963fbc7735
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-08-30 12:38:39 +1000

    watch diff

--- composition/App.vue
+++ composition/App.vue
@@ -1,8 +1,10 @@
 <template>
   <button @click="increment">{{ count }}</button>
-  <button @click="increase('a')">{{ numbers.a }}</button>
-  <button @click="increase('b')">{{ numbers.b }}</button>
-  {{ total }}
+  <button @click="a++">{{ a }}</button>
+  <button @click="b++">{{ b }}</button>
+  <div v-for="msg in history">
+    {{ msg }}
+  </div>
 </template>
 
 <script>
@@ -12,34 +14,30 @@ export default {
   setup() {
     // single number, string
     const count = ref(0)
-
-    // complex
-    const numbers = reactive({
-      a: 0,
-      b: 0
-    })
-
-    const increase = (num) => {
-      numbers[num] += 1
-    }
+    const a = ref(0)
+    const b = ref(0)
+    const history = ref([])
 
     const increment = () => {
       count.value += 1
     }
 
+    watch([a, b], ([newA, newB], [oldA, oldB]) => {
+      if (newA !== oldA) {
+        history.value.push(`a: ${oldA} -> ${newA}`)
+      }
 
-    watchEffect(() => {
-      console.log(numbers.a)
+      if (newB !== oldB) {
+        history.value.push(`b: ${oldB} -> ${newB}`)
+      }
     })
 
-    const total = computed(() => count.value + numbers.a + numbers.b)
-
     return {
-      total,
+      history,
       increment,
-      increase,
       count,
-      numbers
+      a,
+      b
     }
   }
 }
