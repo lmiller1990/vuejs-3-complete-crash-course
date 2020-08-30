8d82973202a018bb005d993ccdb2cc20d7c0ddb3
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-08-30 11:25:52 +1000

    composition api

---
+++ composition/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="increment">{{ count }}</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+
+    const increment = () => {
+      count.value += 1
+    }
+
+    return {
+      increment,
+      count
+    }
+  }
+}
+</script>
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import PokemonApp from './pokemon/App.vue'
+import CompositionApp from './composition/App.vue'
 
-createApp(PokemonApp).mount('#app')
+createApp(CompositionApp).mount('#app')
