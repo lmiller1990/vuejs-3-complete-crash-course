c4eb1cb107c24814393fef2820ccbb9847119621
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-08-30 18:14:47 +1000

    update posts

--- router/Post.vue
+++ router/Post.vue
@@ -1,11 +1,31 @@
 <template>
-  <h4>Title</h4>
-  <p>
-    Content
-  </p>
+  <div v-if="post">
+    <h4>{{ post.title }}</h4>
+    <p>
+    {{ post.content }}
+    </p>
+  </div>
 </template>
 
 <script>
+import { computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router/dist/vue-router.esm.js'
+import { testPosts } from '../microblog/testPosts.js'
+
 export default {
+  setup() {
+    const route = useRoute()
+    console.log(route.params)
+
+    const post = computed(() => {
+      return testPosts.find(
+        x => x.id === parseInt(route.params.id)
+      )
+    })
+
+    return {
+      post
+    }
+  }
 }
 </script>
--- router/Posts.vue
+++ router/Posts.vue
@@ -1,9 +1,22 @@
 <template>
   <h4>Posts</h4>
+  <router-link 
+    v-for="post in testPosts"
+    :to="`/posts/${post.id}`"
+  >
+    {{ post.title }}
+  </router-link>
   <router-view />
 </template>
 
 <script>
+import { testPosts } from '../microblog/testPosts.js'
+
 export default {
+  setup() {
+    return {
+      testPosts
+    }
+  }
 }
 </script>
