d85cbfac51a57b4e04dbd4885b992f05f75a62b7
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-08-30 11:32:56 +1000

    reactive

--- composition/App.vue
+++ composition/App.vue
@@ -1,13 +1,26 @@
 <template>
   <button @click="increment">{{ count }}</button>
+  <button @click="increase('a')">{{ numbers.a }}</button>
+  <button @click="increase('b')">{{ numbers.b }}</button>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
   setup() {
+    // single number, string
     const count = ref(0)
+
+    // complex
+    const numbers = reactive({
+      a: 0,
+      b: 0
+    })
+
+    const increase = (num) => {
+      numbers[num] += 1
+    }
 
     const increment = () => {
       count.value += 1
@@ -15,7 +28,9 @@ export default {
 
     return {
       increment,
-      count
+      increase,
+      count,
+      numbers
     }
   }
 }
