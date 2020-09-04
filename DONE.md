84639d64144c0f854c76e2f32ebb4f5db9c71bd6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-09-04 23:06:52 +1000

    add caching

---
+++ DONE.md
@@ -0,0 +1,36 @@
+What's next?
+
++------------+
+| Photos App |
++------------+
+
+- loading states with Vuex
+- make it look good!
+- more features? "liked" photos - you could use localStorage
+
++--------------------+
+| As a Vue developer |
++--------------------+
+
+- try using vue-cli (instead of Vite)
+- rewrite all the applications using both Options and Composition APIs
+- understand them both
+- my advanced course, Vue.js: The Composition API (TypeScript, unit testing, build your own Vuex)
+
++--------------------------+
+| As a front-end developer |
++------------------------=-+
+
+- keep challenging yourself!
+- GitHub - post a lot, don't feel "bad" about your code
+- contribute to your favorite projects
+  - blog post
+  - issue
+  - helping people out
+
+- join Vue-land discord
+  - message me anytime!
+  - @Lachlan19900
+  - email
+  - https://vuejs-course.com
+
--- photos/photos.js
+++ photos/photos.js
@@ -3,21 +3,37 @@ export const photos = {
 
   state() {
     return {
-      currentAlbumPhotos: []
+      currentAlbumPhotos: [],
+      loading: false,
+      photoCache: {}
     }
   },
 
   mutations: {
-    setPhotos(state, photos) {
+    setPhotos(state, { photos, albumId }) {
       state.currentAlbumPhotos = photos
+      state.photoCache[albumId] = photos
+    },
+
+    setLoading(state, loading) {
+      state.loading = loading
     }
   },
 
   actions: {
     async fetchPhotosForAlbum(ctx, { id }) {
+      if (ctx.state.photoCache[id]) {
+        ctx.commit('setPhotos', { photos: ctx.state.photoCache[id], albumId: id })
+        return
+      }
+
+      ctx.commit('setLoading', true)
+
       const res = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
       const data = await res.json()
-      ctx.commit('setPhotos', data)
+      ctx.commit('setPhotos', { photos: data, albumId: id })
+
+      ctx.commit('setLoading', false)
     }
   }
 }
